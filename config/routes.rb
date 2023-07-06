28e4da9ade45bf3397fc14433874a7339036127e
Yassar Ahmed <yassarahmed17@gmail.com>
2023-07-06 13:05:21 -0500

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/all_products", controller: "products", action: "all_products"
+  get "/first_product", controller: "products", action: "first_product"
+  get "/second_product", controller: "products", action: "second_product"
+  get "/third_product", controller: "products", action: "third_product"
 end
