3695564c0866e6cab75a35b348363b89d4b42e85
Yassar Ahmed <yassarahmed17@gmail.com>
2023-07-10 12:40:30 -0500

    adds destroy method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,12 +22,18 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find_by(id: params["id"])
-    @product = Product.update(
-      name: params["name"],
-      price: params["price"],
-      image_url: params["image_url"],
-      description: params["description"],
-
+    @product.update(
+      name: params["name"] || @product.name,
+      price: params["price"] || @product.price,
+      image_url: params["image_url"] || @product.image_url,
+      description: params["description"] || @product.description,
     )
+    render :show
+  end
+
+  def destroy
+    product = Product.find_by(id: params["id"])
+    product.destroy
+    render json: { message: "Product Destroyed!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/all_products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
-  patch "/products:id" => "products#update"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
