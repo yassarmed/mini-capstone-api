ab02299e4b98c81dc80f5110e0d1beb17bd958b3
Yassar Ahmed <yassarahmed17@gmail.com>
2023-07-10 11:36:28 -0500

    adds update method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,33 @@
 class ProductsController < ApplicationController
-  def all_products
+  def index
     @products = Product.all
     render template: "products/index"
   end
 
-  def first_product
-    @product = Product.first
-    render template: "products/show"
+  def show
+    @product = Product.find_by(id: params["id"])
+    render template: show
   end
 
-  def second_product
-    @product = Product.second
-    render template: "products/show"
+  def create
+    @product = Product.create(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+
+    )
+    render :show
   end
 
-  def third_product
-    @product = Product.third
-    render template: "products/show"
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product = Product.update(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "/all_products", controller: "products", action: "all_products"
-  get "/first_product", controller: "products", action: "first_product"
-  get "/second_product", controller: "products", action: "second_product"
-  get "/third_product", controller: "products", action: "third_product"
+  get "/all_products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products:id" => "products#update"
 end
