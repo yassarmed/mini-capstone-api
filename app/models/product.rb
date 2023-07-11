ee5a9c771324ee260770ef2d5c82059279912f89
Yassar Ahmed <yassarahmed17@gmail.com>
2023-07-11 14:21:24 -0500

    adds tax and total method models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,16 @@
 class Product < ApplicationRecord
   def is_discounted?
     result = ""
-    result += "discounted" if price < 10
-    result += "not discounted" if price >= 10
+    result += "true" if price < 10
+    result += "false" if price >= 10
     result
+  end
+
+  def tax
+    return(price * 0.09)
+  end
+
+  def total
+    return(price + tax)
   end
 end
